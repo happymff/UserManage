046d9a150eed09d4cb3f9165c6161cc31a362043
mengfeifei <happymff@126.com>
2017-10-31 14:42:13 +0800

    update mysql connect method to set the encoding

--- src/main/java/UserManager/MysqlConnect.java
+++ src/main/java/UserManager/MysqlConnect.java
@@ -34,55 +34,6 @@ public class MysqlConnect {
         return conn;
     }
 
-//    public static PreparedStatement insert(String s, Connection conn) {
-//        String[] strings = s.split("\n");
-//        PreparedStatement statement1 = null;
-//        try {
-//            for (String insertString : strings) {
-//                // 要执行的SQL语句
-//                String sql1 = "insert into userInfo values(" + insertString + ")";
-//                // Preparestatement用来执行SQL的insert语句
-//                statement1 = conn.prepareStatement(sql1);
-//                int count = statement1.executeUpdate();
-//                if (count > 0) {
-//                    System.out.println("插入成功！");
-//                } else {
-//                    System.out.println("插入失败！");
-//                }
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return statement1;
-//    }
-//
-//    public static ResultSet select(Connection conn, String userid, String username) {
-//        ResultSet rs = null;
-//        try {
-//            // statement用来执行SQL语句
-//            Statement statement = conn.createStatement();
-//            // 要执行的SQL语句
-//            String sql = "select * from userInfo";
-//            // 结果集
-//            rs = statement.executeQuery(sql);
-//            while (rs.next()) {
-//                // 选择runResult这列数据
-//                String userid1 = rs.getString(userid);
-//                String username1 = rs.getString(username);
-//                // 输出结果
-//                System.out.println("userid:" + "\t" + userid1 + "\t" + "username:" + "\t" + username1);
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return rs;
-//
-//    }
-//
    /**
      * 关闭conn
      *
@@ -128,8 +79,7 @@ public class MysqlConnect {
 
 
     public static void main(String[] args) {
+        //测试数据库连接
         Connection connection=connectMysql();
-
-
     }
 }
